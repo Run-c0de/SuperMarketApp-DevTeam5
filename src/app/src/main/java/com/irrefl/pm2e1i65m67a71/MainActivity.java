9d04617bdf9e2aca41ae0cf93ae289139f7fa1ec
Angie-Martinezz <123768282+Angie-Martinezz@users.noreply.github.com>
2024-02-15 21:44:28 -0600

    Creating Project
    project created

---
+++ src/app/src/main/java/com/irrefl/pm2e1i65m67a71/MainActivity.java
@@ -0,0 +1,15 @@
+package com.irrefl.pm2e1i65m67a71;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //hola
+    }
+}
